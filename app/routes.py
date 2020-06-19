bec6feb6e7d47ba136e681cf7997f33a39f0fa09
ASHSIH-24 <ashisingla.24@gmail.com>
2020-06-19 15:41:23 +0530

    'better'

--- app/routes.py
+++ app/routes.py
@@ -78,9 +78,9 @@ def logout():
 	logout_user()
 	return redirect(url_for('index'))
 	
-@app.route('/index/blogs', methods=['GET','POST'])
+@app.route('/index/explore', methods=['GET','POST'])
 @login_required
-def blogs():
+def explore():
 	form = BlogForm()
 	
 	if form.validate_on_submit():
@@ -92,18 +92,24 @@ def blogs():
 		db.session.add(blog)
 		db.session.commit()
 		flash('Your thought posted successfully!')
-		return redirect(url_for('blogs'))
+		return redirect(url_for('explore'))
 		
 	page  = request.args.get('page', 1, type=int)	
 	
 	posts = Blog.query.order_by(Blog.time_stamp.desc()).paginate(page, app.config['POST_PER_PAGE'], False)
 	
-	next_url = url_for('blogs', page=posts.next_num) if posts.has_next else None
+	next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
 		
-	prev_url = url_for('blogs', page=posts.prev_num) if posts.has_prev else None
+	prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
 		
-	return render_template('blogs.html', title='TP-Blogs', posts=posts.items, next_url=next_url, prev_url=prev_url,\
+	return render_template('explore.html', title='TP-Blogs', posts=posts.items, next_url=next_url, prev_url=prev_url,\
 							form = form)
+	
+@app.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+	user = User.query.filter_by(username=username).first()
+	return render_template('user_popup.html', user=user)
 	
 @app.route('/user/<username>')
 @login_required
